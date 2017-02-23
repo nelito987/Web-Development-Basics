772d7a2581eaa8d790fbb6d05c9cde66a4f1aa29
nelito987 <nelito987@abv.bg>
2017-02-23 19:00:33 +0200

    adding Sharp Store MVC
    sharp store MVC beggining

---
+++ SharpStore MVC/SharpStore/Views/Home/About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SimpleMVC.Interfaces;
+using System.IO;
+
+namespace SharpStore.Views.Home
+{
+    class About : IRenderable
+    {
+        public string Render()
+        {
+            return File.ReadAllText("../../content/about.html");
+        }
+    }
+}
---
+++ SharpStore MVC/SharpStore/Views/Home/Index.cs
@@ -0,0 +1,14 @@
+﻿using SimpleMVC.Interfaces;
+using System.IO;
+
+namespace SharpStore.Views.Home
+{
+    public class Index : IRenderable
+    {
+        public string Render()
+        {
+            return File.ReadAllText("../../content/home.html");
+        }
+    }
+}
+
